e38874770a77253ea36c551e946150c2ba99efe0
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-03-01 04:48:52 +0000

    Eyy

--- script/ask.js
+++ script/ask.js
@@ -2,41 +2,28 @@ const axios = require('axios');
 
 module.exports.config = {
 		name: "ask",
-		version: "1.0.0",
-		role: 0,
-		credits: "Eugene Aguilar",
-		description: "Interacts with a GPT-4 API",
-		cooldown: 5,
+		version: 1.0,
+		credits: "OtinXSandip",
+		description: "AI",
 		hasPrefix: false,
-		aliases: ["gpt", "Ai",],
-		usages: "",
+		usages: "{pn} [prompt]",
+		aliases: ["gpt4"],
+		cooldown: 0,
 };
 
-module.exports.run = async function ({ api, event }) {
-		const args = event.body.split(/\s+/);
-		args.shift(); // Remove the command itself from the arguments
-
-		// Check if there are arguments
-		if (args.length === 0) {
-				api.sendMessage("Please provide a question or prompt.", event.threadID, event.messageID);
-				return;
-		}
-
-		const apiUrl = `https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(args.join(' '))}`;
-
+module.exports.run = async function ({ api, event, args }) {
 		try {
-				const response = await axios.get(apiUrl);
-
-				// Check if the API response contains valid data
-				if (response.status === 200 && response.data && response.data.trim() !== "") {
-						const answer = response.data.trim();
-						api.sendMessage(answer, event.threadID, event.messageID);
-				} else {
-						console.error("Invalid or empty AI response");
-						api.sendMessage("Invalid or empty AI response", event.threadID, event.messageID);
+				const prompt = args.join(" ");
+				if (!prompt) {
+						await api.sendMessage("Hey I'm your virtual assistant, ask me a question.", event.threadID);
+						return;
 				}
+
+				const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+				const answer = response.data.answer;
+
+				await api.sendMessage(answer, event.threadID);
 		} catch (error) {
-				console.error("Error fetching AI response:", error);
-				api.sendMessage("Error fetching AI response", event.threadID, event.messageID);
+				console.error("Error:", error.message);
 		}
 };
