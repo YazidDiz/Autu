966234ebb49f0304fd29c910c8f5f16b1a514281
YazidDiz <154859867+YazidDiz@users.noreply.github.com>
2024-03-02 13:13:21 +0100

    Update pinterest.js

--- script/pinterest.js
+++ script/pinterest.js
@@ -5,7 +5,7 @@ module.exports.config = {
   name: "pinterest",
   version: "1.0.0",
   role: 0,
-  hasPrefix: true,
+  hasPrefix: false,
   description: "Search for images on Pinterest.",
   usages: "pinterest [query] - [amount]",
   credits: "Developer",
